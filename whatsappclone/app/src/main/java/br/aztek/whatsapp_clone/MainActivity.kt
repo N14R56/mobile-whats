948124bd0525628c9fc58347ec84189b9f1edb5b
N14R56 <2.0turbocoder@gmail.com>
2022-09-29 07:21:56 -0300

    feat: redirecione uma mensagem em um numero de whatsapp

---
+++ whatsappclone/app/src/main/java/br/aztek/whatsapp_clone/MainActivity.kt
@@ -0,0 +1,27 @@
+package br.aztek.whatsapp_clone
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val phoneNumber : EditText = findViewById(R.id.phoneNumber)
+        val message : EditText = findViewById(R.id.message)
+        val btn : Button = findViewById(R.id.sendButton)
+
+        btn.setOnClickListener {
+            val number = phoneNumber.text.toString()
+            val msg = message.text.toString()
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse("https://api.whatsapp.com/send?phone=$number&text=$msg")
+            startActivity(intent)
+        }
+    }
+}
